4127348f1c0dc5dac39b35b36def3f32f59aa0d7
akolt19d <90689597+akolt19d@users.noreply.github.com>
2021-11-28 18:29:49 +0100

    Dodanie fazy ustawiania statków
    -dodano opcje ustawienia statków
    -dodano możliwosć obrócenia statków

--- game.js
+++ game.js
@@ -4,8 +4,9 @@ let roundTime = localStorage.getItem("roundTime");
 let opponent;
 
 const opponents = ["ELMENOL", "SMILEDOG", "case_meen", "_pikus", "Spectral", "Cheppe", "lewek", "AbcightGaming"];
+const field = document.querySelector("#field")
 
-function liczbaLosowa(max) {
+function randomInt(max) {
     return Math.floor(Math.random()*max);
 }
 
@@ -45,15 +46,125 @@ function startAnimation()
         }, 1500);
 }
 
+function isFleetFull()
+{
+    let shipSum = 0;
+    let isFull = false;
+
+    for(let i = 0; i < 4; i++)
+    {
+        shipSum += parseInt(document.querySelectorAll("#position_container div p")[i].innerText[0])
+    }
+
+    console.log(shipSum)
+
+    if(shipSum == 0)
+    {
+        isFull = true;
+    }
+
+    return isFull;
+}
+
+function positioningShips(shipsNum)
+{
+    for(let i = 0; i < 64; i++)
+    {
+        const div = document.createElement("div")
+        div.className = "field_grid";
+        if(i < 9)
+        {
+            div.id = `field_grid0${i+1}`;
+        }
+        else
+        {
+            div.id = `field_grid${i+1}`;
+        }
+        field.append(div);
+    }
+    
+    let rotation = 8;
+    document.querySelector("#rt_right").addEventListener('click', () => { rotation = 1 })
+    document.querySelector("#rt_bottom").addEventListener('click', () => { rotation = 8 })
+
+    let fleetPosition = [];
+
+    for(let i = 0; i < 4; i++)
+    {
+        document.querySelectorAll("#position_container div p")[i].addEventListener('click', (e)=>{
+            let shipAmount = parseInt(e.currentTarget.innerText[0])
+            let shipSize = parseInt(e.currentTarget.innerText[e.currentTarget.innerText.length-1])
+            
+            if(shipAmount == 0)
+            {
+                //console.log("essa")
+                console.log(fleetPosition[0] + fleetPosition[1])
+            }
+            else
+            {
+                console.log(shipSize)
+
+                for(let i = 0; i < 64; i++)
+                {
+                    let startingPosition = 0;
+                    document.querySelectorAll(".field_grid")[i].addEventListener('click', function positioningShips(e){
+                    startingPosition = parseInt(e.currentTarget.id[e.currentTarget.id.length - 2] + e.currentTarget.id[e.currentTarget.id.length - 1]);
+    
+                    for(let i = startingPosition; i < startingPosition + shipSize * rotation; i += rotation)
+                    {
+                        console.log(i);
+                        if(i <= 9)
+                        {
+                            document.querySelector(`#field_grid0${i}`).style.backgroundColor = "var(--color-green)";
+                            document.querySelector(`#field_grid0${i}`).style.border = "0.2em solid black";
+                        }
+                        else 
+                        {
+                            document.querySelector(`#field_grid${i}`).style.backgroundColor = "var(--color-green)";
+                            document.querySelector(`#field_grid${i}`).style.border = "0.2em solid black";
+                        }
+                        fleetPosition += i;
+                    }
+    
+                    for(let i = 0; i < 64; i++)
+                    {
+                        document.querySelectorAll(".field_grid")[i].removeEventListener('click', positioningShips)
+                    }
+                    
+                    startingPosition = 0;
+                    shipAmount = 0;
+                    shipSize = 0;
+                    let isFull = isFleetFull()
+                    if(isFull == true)
+                    {
+                        document.querySelector("#inner_container > div:last-child").style.display = "none";
+                        document.querySelector("#position_container").style.opacity = 0;
+                        setTimeout(() => {
+                            document.querySelector("#position_container").style.display = "none";
+                        }, 2000);
+                    }
+                    })
+                }
+                shipAmount = parseInt(e.currentTarget.innerText[0])
+                e.currentTarget.innerText = `${shipAmount-1}x${shipSize}`;
+            }
+        });
+    }
+
+    // document.querySelector("#position_container").addEventListener('click', ()=>{
+    //     document.querySelector("#position_container").style.height = "5em"
+    // })
+}
+
 window.addEventListener('load', () => {
-    opponent = opponents[liczbaLosowa(opponents.length)];
+    opponent = opponents[randomInt(opponents.length)];
 
     document.querySelector("#nav_player_name").innerText = username;
     document.querySelector("#nav_opp_name").innerText = opponent;
     document.querySelector("title").innerText = `${username} vs ${opponent}`;
 
-    setTimeout(startAnimation, 1250);
+    //setTimeout(startAnimation, 1250);
     
-
+    positioningShips(shipsNum)
 })
 
