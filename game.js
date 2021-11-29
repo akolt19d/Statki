8876968f81f6e91d275a5e3d7c5944371ad1e1a7
akolt19d <90689597+akolt19d@users.noreply.github.com>
2021-11-29 19:58:07 +0100

    Dokończenie game loop, dodanie animacji końcowej
    -dodano game loop(wybieranie pola przeciwnika, przeciwnik wybiera pole gracza)
    -dodano animacje kończącą rozgrywkę

--- game.js
+++ game.js
@@ -2,18 +2,35 @@ let username = localStorage.getItem("username");
 let shipsNum = localStorage.getItem("shipsNum");
 let roundTime = localStorage.getItem("roundTime");
 let opponent;
+let startGame = false;
+let playerShipsSunk = 0;
+let oppShipsSunk = 0;
+let roundNum = 0;
+let gameOver = false;
+let fleetPosition = [];
+let enemyFleet = generateEnemyFleet();
+let flag = 0;
+let flagSequel = 0;
 
 const opponents = ["ELMENOL", "SMILEDOG", "case_meen", "_pikus", "Spectral", "Cheppe", "lewek", "AbcightGaming"];
 const field = document.querySelector("#field")
+const terminal = document.querySelector("#terminal")
+const wrapper = document.querySelector("#wrapper");
 
 function randomInt(max) {
     return Math.floor(Math.random()*max);
 }
 
+function terminalLog(text)
+{
+    const p = document.createElement("p")
+    p.innerText = text;
+    terminal.append(p)
+}
+
 function startAnimation()
 {
-    const wrapper = document.querySelector("#wrapper");
-    const template = document.querySelector("template");
+    const template = document.querySelectorAll("template")[0];
 
     const cnt = template.content.cloneNode(true);
 
@@ -56,7 +73,7 @@ function isFleetFull()
         shipSum += parseInt(document.querySelectorAll("#position_container div p")[i].innerText[0])
     }
 
-    console.log(shipSum)
+    //console.log(shipSum)
 
     if(shipSum == 0)
     {
@@ -66,7 +83,239 @@ function isFleetFull()
     return isFull;
 }
 
-function positioningShips(shipsNum)
+function generateEnemyFleet()
+{
+    let enemyFleet = []
+    let fieldNum = []
+    
+    for(let i = 1; i <= 64; i++)
+    {
+        fieldNum[i-1] = i
+    }
+    
+    for(let i = 16; i < 20; i+=4)
+    {
+        let random = fieldNum[randomInt(fieldNum.length)]
+    
+        enemyFleet[i] = random - 1
+        enemyFleet[i+1] = random
+        enemyFleet[i+2] = random + 1
+        enemyFleet[i+3] = random + 2
+
+        fieldNum.splice(random-2, 1)
+        fieldNum.splice(random-1, 1)
+        fieldNum.splice(random, 1)
+        fieldNum.splice(random+1, 1)
+    }
+
+    for(let i = 10; i < 16; i+=3)
+    {
+        let random = fieldNum[randomInt(fieldNum.length)]
+
+        enemyFleet[i] = random - 1
+        enemyFleet[i+1] = random
+        enemyFleet[i+2] = random + 1
+
+        fieldNum.splice(random-2, 1)
+        fieldNum.splice(random-1, 1)
+        fieldNum.splice(random, 1)
+    }
+
+    for(let i = 4; i < 10; i+=2)
+    {
+        let random = fieldNum[randomInt(fieldNum.length)]
+
+        enemyFleet[i] = random
+        enemyFleet[i+1] = random + 1
+
+        fieldNum.splice(random-1, 1)
+        fieldNum.splice(random, 1)
+    }
+
+    for(let i = 0; i < 4; i++)
+    {
+        let random = fieldNum[randomInt(fieldNum.length)]
+        //console.log(random)
+
+        enemyFleet[i] = random
+        fieldNum.splice(random-1, 1)
+    }
+    //console.log(fieldNum)
+    //console.log(enemyFleet)
+
+    return enemyFleet;
+}
+
+function isGameOver()
+{
+    if(oppShipsSunk >= enemyFleet.length || playerShipsSunk >= fleetPosition.length)
+        return true;
+    else
+        return false;
+}
+
+function oppTurn()
+{
+    if(flagSequel == 0)
+    {
+        document.querySelector("#field").style.display = "grid"
+        document.querySelector("#opp_field").style.display = "none"
+        document.querySelector("#field_name").innerText = "Twoja plansza"
+
+        let grids = []
+        let isPlayerField = false;
+        for(let i = 1; i <= 64; i++)
+        {
+            grids[i-1]=  i;
+        }
+        //console.log(`przed usunięciem: ${grids}`)
+    
+        let oppGuess = grids[randomInt(grids.length)];
+        //console.log(oppGuess)
+    
+        for(let i = 0; i < 64; i++)
+        {
+            if(oppGuess == grids[i])
+                grids.splice(i, 1)
+        }
+    
+        for(let i = 0; i < fleetPosition.length; i++)
+        {
+            if(oppGuess == fleetPosition[i])
+            {
+                isPlayerField = true;
+            }
+        }
+    
+        if(isPlayerField)
+        {
+            if(oppGuess < 10)
+            {
+                document.querySelector(`#field_grid0${oppGuess}`).style.backgroundColor = "red"
+                document.querySelector(`#field_grid0${oppGuess}`).style.border = "0.2em solid black"
+            }
+            else
+            {
+                document.querySelector(`#field_grid${oppGuess}`).style.backgroundColor = "red"
+                document.querySelector(`#field_grid${oppGuess}`).style.border = "0.2em solid black"
+            }
+            terminalLog(`${opponent} zatopił statek!`)
+            playerShipsSunk++;
+        }
+        else
+        {
+            if(oppGuess < 10)
+            {
+                document.querySelector(`#field_grid0${oppGuess}`).style.backgroundColor = "#0e0e0e"
+                document.querySelector(`#field_grid0${oppGuess}`).style.border = "0.2em solid black"
+            }
+            else
+            {
+                document.querySelector(`#field_grid${oppGuess}`).style.backgroundColor = "#0e0e0e"
+                document.querySelector(`#field_grid${oppGuess}`).style.border = "0.2em solid black"
+            }
+            terminalLog(`${opponent} nie trafił.`)
+        }
+        //console.log(`po usunięciu: ${grids}`)
+        //console.log(`Wynik: ${oppShipsSunk} - ${playerShipsSunk}`)
+    
+        gameOver = isGameOver()
+        flag = 0;
+        setTimeout(playerTurn, 2000);
+        flagSequel++
+        //console.log(flag)
+    }
+}
+
+function selectOppField(e){
+    let clickedField = parseInt(e.currentTarget.id[e.currentTarget.id.length - 2]+e.currentTarget.id[e.currentTarget.id.length - 1])
+    let isOppField = false;
+
+    for(let j = 0; j < enemyFleet.length; j++)
+    {
+        if(clickedField == enemyFleet[j])
+            isOppField = true;
+    }
+
+    if(isOppField)
+    {
+        e.currentTarget.style.backgroundColor = "var(--color-green)"
+        e.currentTarget.style.border = "0.2em solid black"
+        terminalLog(`${username} zatopił statek!`)
+        oppShipsSunk++;
+    }
+    else
+    {
+        e.currentTarget.style.backgroundColor = "red"
+        e.currentTarget.style.border = "0.2em solid black"
+        terminalLog(`${username} nie trafił.`)
+    }
+
+    for(let j = 0; j < 64; j++)
+    {
+        document.querySelectorAll(".opp_field_grid")[j].removeEventListener('click', selectOppField)
+    }
+
+    gameOver = isGameOver();
+    flagSequel = 0;
+    setTimeout(oppTurn, 2000);
+    flag++
+    //console.log(flagSequel)
+}
+
+function playerTurn()
+{
+    if(flag == 0)
+    {
+        document.querySelector("#field").style.display = "none"
+        document.querySelector("#opp_field").style.display = "grid"
+        document.querySelector("#field_name").innerText = "Plansza przeciwnika"
+        roundNum++;
+        document.querySelector("#timer h3").innerText = `runda ${roundNum}`
+        for(let i = 0; i < 64; i++)
+            document.querySelectorAll(".opp_field_grid")[i].addEventListener('click', selectOppField)
+    }
+}
+
+function gameOverAnimation()
+{
+    const template = document.querySelectorAll("template")[1];
+
+    const cnt = template.content.cloneNode(true);
+
+    wrapper.appendChild(cnt);
+
+    if(playerShipsSunk > oppShipsSunk)
+    {
+        document.querySelector("#end_popup section:first-child h1").innerText = "Przegrałeś :("
+    }
+    document.querySelector("#end_popup_player_name").innerText = username
+    document.querySelector("#end_popup_opp_name").innerText = opponent
+    document.querySelector("#end_popup section:last-of-type h2:nth-child(2)").innerText = `${oppShipsSunk}-${playerShipsSunk}`
+    document.querySelector("#end_popup section:last-of-type h2:last-child").innerText = `${roundNum}`
+    document.querySelector("#end_popup").style.animation = "vis 1s linear forwards";
+    document.querySelector("#exit_game").addEventListener('click', () => {
+        window.location.replace("./index.html");
+    })
+}
+
+function gameLoop()
+{
+    console.log(enemyFleet)
+    
+    playerTurn()
+
+    let gameOverWatcher = setInterval(() => {
+        if(gameOver)
+        {
+            setTimeout(gameOverAnimation, 1250);
+            clearInterval(gameOverWatcher)
+        }
+    }, 1000);
+}
+
+
+function positioningShips()
 {
     for(let i = 0; i < 64; i++)
     {
@@ -82,12 +331,25 @@ function positioningShips(shipsNum)
         }
         field.append(div);
     }
+
+    for(let i = 0; i < 64; i++)
+    {
+        const div = document.createElement("div")
+        div.className = "opp_field_grid";
+        if(i < 9)
+        {
+            div.id = `opp_field_grid0${i+1}`;
+        }
+        else
+        {
+            div.id = `opp_field_grid${i+1}`;
+        }
+        document.querySelector("#opp_field").append(div);
+    }
     
     let rotation = 8;
     document.querySelector("#rt_right").addEventListener('click', () => { rotation = 1 })
     document.querySelector("#rt_bottom").addEventListener('click', () => { rotation = 8 })
-
-    let fleetPosition = [];
 
     for(let i = 0; i < 4; i++)
     {
@@ -97,52 +359,53 @@ function positioningShips(shipsNum)
             
             if(shipAmount == 0)
             {
-                //console.log("essa")
-                console.log(fleetPosition[0] + fleetPosition[1])
+                console.log(fleetPosition[0])
+                console.log(fleetPosition)
             }
             else
             {
-                console.log(shipSize)
-
+                //console.log(shipSize)
+                
                 for(let i = 0; i < 64; i++)
                 {
                     let startingPosition = 0;
                     document.querySelectorAll(".field_grid")[i].addEventListener('click', function positioningShips(e){
-                    startingPosition = parseInt(e.currentTarget.id[e.currentTarget.id.length - 2] + e.currentTarget.id[e.currentTarget.id.length - 1]);
-    
-                    for(let i = startingPosition; i < startingPosition + shipSize * rotation; i += rotation)
-                    {
-                        console.log(i);
-                        if(i <= 9)
+                        startingPosition = parseInt(e.currentTarget.id[e.currentTarget.id.length - 2] + e.currentTarget.id[e.currentTarget.id.length - 1]);
+                        
+                        for(let i = startingPosition; i < startingPosition + shipSize * rotation; i += rotation)
                         {
-                            document.querySelector(`#field_grid0${i}`).style.backgroundColor = "var(--color-green)";
-                            document.querySelector(`#field_grid0${i}`).style.border = "0.2em solid black";
+                            //console.log(i);
+                            if(i <= 9)
+                            {
+                                document.querySelector(`#field_grid0${i}`).style.backgroundColor = "var(--color-green)";
+                                document.querySelector(`#field_grid0${i}`).style.border = "0.2em solid black";
+                            }
+                            else 
+                            {
+                                document.querySelector(`#field_grid${i}`).style.backgroundColor = "var(--color-green)";
+                                document.querySelector(`#field_grid${i}`).style.border = "0.2em solid black";
+                            }
+                            fleetPosition[fleetPosition.length] = i;
                         }
-                        else 
+                        
+                        for(let i = 0; i < 64; i++)
                         {
-                            document.querySelector(`#field_grid${i}`).style.backgroundColor = "var(--color-green)";
-                            document.querySelector(`#field_grid${i}`).style.border = "0.2em solid black";
+                            document.querySelectorAll(".field_grid")[i].removeEventListener('click', positioningShips)
                         }
-                        fleetPosition += i;
-                    }
-    
-                    for(let i = 0; i < 64; i++)
-                    {
-                        document.querySelectorAll(".field_grid")[i].removeEventListener('click', positioningShips)
-                    }
-                    
-                    startingPosition = 0;
-                    shipAmount = 0;
-                    shipSize = 0;
-                    let isFull = isFleetFull()
-                    if(isFull == true)
-                    {
-                        document.querySelector("#inner_container > div:last-child").style.display = "none";
-                        document.querySelector("#position_container").style.opacity = 0;
-                        setTimeout(() => {
-                            document.querySelector("#position_container").style.display = "none";
-                        }, 2000);
-                    }
+                        
+                        startingPosition = 0;
+                        shipAmount = 0;
+                        shipSize = 0;
+                        let isFull = isFleetFull()
+                        if(isFull == true)
+                        {
+                            document.querySelector("#inner_container > div:last-child").style.display = "none";
+                            document.querySelector("#position_container").style.opacity = 0;
+                            setTimeout(() => {
+                                document.querySelector("#position_container").style.display = "none";
+                            }, 2000);
+                            startGame = true;
+                        }
                     })
                 }
                 shipAmount = parseInt(e.currentTarget.innerText[0])
@@ -150,10 +413,6 @@ function positioningShips(shipsNum)
             }
         });
     }
-
-    // document.querySelector("#position_container").addEventListener('click', ()=>{
-    //     document.querySelector("#position_container").style.height = "5em"
-    // })
 }
 
 window.addEventListener('load', () => {
@@ -165,6 +424,16 @@ window.addEventListener('load', () => {
 
     //setTimeout(startAnimation, 1250);
     
-    positioningShips(shipsNum)
+    positioningShips()
+
+    let checkGameStart = setInterval(() => {
+        if(startGame == true)
+        {
+            gameLoop();
+            terminalLog("Floty zostały rozstawione")
+            clearInterval(checkGameStart);
+            //console.log(fleetPosition)
+        }
+    }, 10);
 })
 
